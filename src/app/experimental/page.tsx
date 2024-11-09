329b3f3d15c97ba9dfd5aa69b130ecd0f6b4fb7d
ashigirl96 <reon@mma.cs.tsukuba.ac.jp>
2024-11-09 21:37:07 +0900

    wip

--- src/app/experimental/page.tsx
+++ src/app/experimental/page.tsx
@@ -25,8 +25,9 @@ export default function App(): JSX.Element {
         <button
           className="rounded bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-700"
           onClick={executeCommand}
+          type={'button'}
         >
-          Execute Command
+          <div> Execute Command</div>
         </button>
       </div>
       <div className="max-w-[800px]">
